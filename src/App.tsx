497f8ef8381ad88e384f8321db0ac0814d80b2a4
enumura1 <akirayuto0718@gmail.com>
2023-09-09 23:28:37 +0900

    add:images on webPage

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,10 @@ import { Canvas } from '@react-three/fiber';
 function Images() {
   return (
     <group>
-      <Image url="./img1.png" scale={[3, 1]} />
+      <Image url="./img1.png" scale={[3, 3]} position={[-3, -2, 0]} />
+      <Image url="./img2.png" scale={[4, 2]} position={[-3, 1, 1]} />
+      <Image url="./img3.png" scale={[3, 2]} position={[1, 1, 2]} />
+      <Image url="./img4.png" scale={[5, 3]} position={[2, -2, 0]} />
     </group>
   );
 }
